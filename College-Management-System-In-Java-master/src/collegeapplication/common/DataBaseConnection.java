27e398535d81a3bbd4002e8ac9c673c2b4045487
Shibam Mohanty <106046613+SHIBAMMOHANTY@users.noreply.github.com>
2024-11-09 01:39:29 +0530

    updated

--- College-Management-System-In-Java-master/src/collegeapplication/common/DataBaseConnection.java
+++ College-Management-System-In-Java-master/src/collegeapplication/common/DataBaseConnection.java
@@ -7,7 +7,7 @@ import java.sql.*;
 public class DataBaseConnection {
 	
 	static Connection con=null;
-	static final String url="jdbc:mysql://localhost:3306/collegedata";
+	static final String url="jdbc:mysql://192.168.1.100:3306/collegedata?useSSL=false&allowPublicKeyRetrieval=true";
 	static final String uname="root";
 	static final String password="";
 	
